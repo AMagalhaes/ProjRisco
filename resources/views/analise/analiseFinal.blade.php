6be709ad3ee90404b99630439d08fd448b1bbcf2
Antonio Magalhaes <antonio_magalhaes1@hotmail.com>
2015-05-15 19:00:18 +0100

    qualquer coisa

--- resources/views/analise/analiseFinal.blade.php
+++ resources/views/analise/analiseFinal.blade.php
@@ -21,37 +21,66 @@
         <th class="col-md-1">Nvl.Risco</th>
         <th class="col-md-1">Categoria</th>
         <th class="col-md-1">Aceitavel</th>
-
-
-
-
-
-
 			</tr>
-
 		</thead>
-
 		<tbody>
       @foreach ($activos as $activo)
+	      <tr>
+	        <td>{!! $activo->nome !!}</td>
+	        </tr>
+	        @foreach ($riscos as $risco)
+			      @if($activo->id == $risco->activo_id)
 
-      <tr>
-        <td>{!! $activo->nome !!}</td>
-        </tr>
-        @foreach ($riscos as $risco)
+						@if ($risco->cat_risco<41)
+						<?php $classe = 'success'; ?>
+						@endif
 
-      @if($activo->id == $risco->activo_id)
-      <tr>
-        <td>{!! '------' !!}</td>
-        <td>{!! 'A',$risco->activo_id,'R',$risco->id !!}</td>
-        <td>{!! $risco->ameaca !!}</td>
-        <td>{!! $risco->vulnerabilidade !!}</td>
-        <td>{!! $risco->probabilidade !!}</td>
-        <td>{!! $risco->impacto !!}</td>
-        <td>{!! $activo->valor !!}</td>
-        <td>{!! ($risco->impacto * $risco->probabilidade * $risco->valor) !!}</td>
-			  </td>
-			</tr>
-      @endif
+						@if (($risco->cat_risco>40) && ($risco->cat_risco<271))
+						<?php $classe = 'active'; ?>
+						@endif
+
+						@if (($risco->cat_risco>270) && ($risco->cat_risco<1121))
+						<?php $classe = 'warning'; ?>;
+						@endif
+
+						@if (($risco->cat_risco>1120) && ($risco->cat_risco<2501))
+						<?php $classe = 'danger'; ?>;
+						@endif
+
+			      <tr class ="{{ $classe }}" >
+			        <td>{!! '------' !!}</td>
+			        <td>{!! 'A',$risco->activo_id,'R',$risco->id !!}</td>
+			        <td>{!! $risco->ameaca !!}</td>
+			        <td>{!! $risco->vulnerabilidade !!}</td>
+			        <td>{!! $risco->probabilidade !!}</td>
+			        <td>{!! $risco->impacto !!}</td>
+			        <td>{!! $activo->valor !!}</td>
+			        <td>{!! $risco->cat_risco !!}</td>
+							@if(($risco->cat_risco>=0) && ($risco->cat_risco<41))
+								<td >{!! 'Aceitavel' !!}</td>
+							@endif
+							@if(($risco->cat_risco>40) && ($risco->cat_risco<271))
+								<td>{!! 'Normal' !!}</td>
+							@endif
+							@if(($risco->cat_risco>270) && ($risco->cat_risco<1120))
+								<td>{!! 'Alto' !!}</td>
+							@endif
+							@if(($risco->cat_risco>1120) && ($risco->cat_risco<2501))
+								<td>{!! 'Critico' !!}</td>
+							@endif
+							@if(($risco->cat_risco>=0) && ($risco->cat_risco<41))
+								<td>{!! 'Sim' !!}</td>
+							@endif
+							@if(($risco->cat_risco>40) && ($risco->cat_risco<271))
+								<td>{!! 'Talvez' !!}</td>
+							@endif
+							@if(($risco->cat_risco>270) && ($risco->cat_risco<1120))
+								<td>{!! 'Não' !!}</td>
+							@endif
+							@if(($risco->cat_risco>1120) && ($risco->cat_risco<2501))
+								<td>{!! 'Não' !!}</td>
+							@endif
+	    @endif
       @endforeach
       @endforeach
 		</tbody>
